63640f36ef75d15fa949a767f958672220ec54cb
RedLavaRus <github@redlava.ru>
2020-07-21 22:33:52 +0300

    добавлен старта шаблона

--- modules/index/view/front/temp.php
+++ modules/index/view/front/temp.php
@@ -1,4 +1,5 @@
 <?php
+/*
 Core\Func\HTML::start();
 Core\Func\HTML::head();
 Core\Func\HTML::startBody();
@@ -11,6 +12,60 @@ Core\Func\HTML::header();
 
 Core\Func\HTML::aside();
 Core\Func\HTML::footer();
-
+*/
 
 ?>
+<!DOCTYPE HTML>
+<html lang="ru">
+    <head>
+        <title>Test</title>
+
+        <meta charset="UTF-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#ff0000">
+
+        <link rel="stylesheet" href="/res/css/default.css">
+        <link rel="icon" sizes="192x192" href="">
+    </head>
+    <body>
+
+
+    <header>
+
+        <nav>
+            <div class="flex2">
+                <div class="logo">LOGO</div>
+            </div>
+            <div class="flex4 flex">
+                <div class="nav_element">Магазин</div>
+                <div class="nav_element">двыхсловный эеменет</div>
+                <div class="nav_element">Мини</div>
+                <div class="nav_element">Очень длинный</div>
+                <div class="nav_element">РАЗДЕЛ1</div>
+                <div class="nav_element">РАЗДЕЛ1</div>
+            </div>
+            <div class="flex4">
+            </div>
+            <div class="flex2">
+                <div class="login">ВХОД</div>
+            </div>
+        </nav>
+
+    </header>
+
+    <main>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </main>
+
+    <aside>
+    </aside>
+
+    <footer>
+    </footer>
+    </body>
+</html>
