0109b9c326607999174586d426b1fba0d271434e
RedLavaRus <github@redlava.ru>
2020-07-28 21:21:03 +0300

    v:0.1.2 Форма авторизации

--- modules/user/config/handler.php
+++ modules/user/config/handler.php
@@ -20,6 +20,9 @@ class Handler
     }
     public function authorization()
     {
-        echo 2;
+        
+        $result = new \Core\Show\View;  
+        $result->add("name_param", 2);
+        $result->view("/modules/user/view/front/authorization.php");
     }
 }
---
+++ modules/user/view/front/authorization.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+    <form method="post" action="">
+        <p><input type="text" name="login" value="" placeholder="Логин"></p>
+        <p><input type="password" name="password1" value="" placeholder="Пароль"></p>
+        <p class="remember_me">
+          <label>
+            <input type="checkbox" name="remember_me" id="remember_me">
+            Запомнить меня
+          </label>
+        </p>
+        <p class="submit"><input type="submit" name="commit" value="Зарегистрироваться"></p>
+      </form>
